0ca37fb7aa72b60427c5f30bea7efafa4aeba9b6
Douglas Wilson <douglas.wilson@quantinuum.com>
2024-01-11 10:15:50 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {pkgs,...}: {
+          languages.rust = {
+            enable = true;
+            channel = "stable";
+            components =
+              [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+          };
+          env = {
+            CC = "${pkgs.stdenv.cc}/bin/cc";
+            CXX = "${pkgs.stdenv.cc}/bin/c++";
+          };
+        };
+      };
+    };
+}
+
